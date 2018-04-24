ca159e1f70c85eed18d702c9d63879854ba0fcbe
leander <leander@wesrc.com>
2018-04-24 22:50:06 +0200

    fix autoloading
    - move Module.php into ZfcUserImpersonate Namespace

---
+++ src/ZfcUserImpersonate/Module.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * ZfcUserImpersonate Module Class
+ *
+ * @created 20130709
+ * @author Mark Tudor <code AT icefusion DOT co DOT uk>
+ */
+
+namespace ZfcUserImpersonate;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\View\Model\ViewModel;
+use ZfcUserImpersonate\Module\AbstractModule;
+
+class Module implements ConfigProviderInterface
+{
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        // TODO: I'm not convinced that this is really the best way to achieve this.
+        // TODO: ZfcUserImpersonate (future) view scripts will have to be in a zfcuser folder, not zfc-user-impersonate.
+        // As a consequence of overriding the ZfcUser User controller, ZF2 will look for all view scripts within
+        // ZfcUserImpersonate's folder. Use the Mvc Dispatch event to point the view script at ZfcUser again so that
+        // we don't have to copy all view scripts from ZfcUser to ZfcUserImpersonate.
+        $eventManager = $e->getApplication()->getEventManager();
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH, function($e) {
+            $model = $e->getResult();
+            if (!$model instanceof ViewModel) {
+                return;
+            }
+            $model->setTemplate(str_replace('zfc-user-impersonate', 'zfc-user', $model->getTemplate()));
+        }, -85);
+    }
+}
